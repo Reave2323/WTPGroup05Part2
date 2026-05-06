ce461f7f1d0a58e50207994682e57cdafa723869
105690697 <105690697@student.swin.edu.au>
2026-05-06 21:23:47 +1000

    removed $conn in settings

--- settings.php
+++ settings.php
@@ -2,13 +2,6 @@
   $host = "localhost";
   $user = "userconn";
   $pwd = "";
-  $sql_db = "jobs_database(1)";
+  $sql_db = "jobs_database";
 ?>
 
-<?php
-  require_once"settings.php";
-    $conn = mysqli_connect("localhost", "userconn", "", "jobs_database");
-    if (!$conn){
-        die("Connection failed:" .mysql_connect_error());
-    }
-?>
