f3375192c926ae5fbf8501a5b0bd9d69c369653e
Reave2323 <jack.whitehall892@gmail.com>
2026-05-06 22:25:13 +1000

    FIXED DB

--- jobs_database.sql
+++ jobs_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 06, 2026 at 11:03 AM
+-- Generation Time: May 06, 2026 at 12:24 PM
 -- Server version: 12.2.2-MariaDB
 -- PHP Version: 8.5.5
 
@@ -22,6 +22,50 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `jobs_database` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `jobs_database`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Eoi`
+--
+
+CREATE TABLE IF NOT EXISTS `Eoi` (
+  `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `reference_number` int(5) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `dob` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` int(10) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `addr` varchar(80) NOT NULL,
+  `country_state` varchar(15) NOT NULL,
+  `skills` varchar(100) NOT NULL,
+  `other_skills` varchar(200) DEFAULT NULL,
+  `post_date` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Jobs`
+--
+
+CREATE TABLE IF NOT EXISTS `Jobs` (
+  `job_id` int(11) NOT NULL AUTO_INCREMENT,
+  `reference_number` int(5) NOT NULL,
+  `job_name` varchar(50) NOT NULL,
+  `job_type` varchar(50) NOT NULL,
+  `job_description` text NOT NULL,
+  `applicant_location` varchar(50) NOT NULL,
+  `salary` int(6) NOT NULL,
+  `responsibilities` text NOT NULL,
+  `essential_req` text NOT NULL,
+  `pref_req` text NOT NULL,
+  `post_date` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`job_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
