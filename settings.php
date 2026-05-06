c895c115cfcf51f57be0880184f67249d731cb0e
105690697 <105690697@student.swin.edu.au>
2026-05-06 21:09:45 +1000

    simple settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,13 @@
 <?php
-    require_once"settings.php";
-    $dbconn = mysql_connect("localhost", "userconn", "", "jobs_database");
+  $host = "localhost";
+  $user = "userconn";
+  $pwd = "";
+  $sql_db = "jobs_database(1)";
+?>
+
+<?php
+  require_once"settings.php";
+    $conn = mysqli_connect("localhost", "userconn", "", "jobs_database");
     if (!$conn){
         die("Connection failed:" .mysql_connect_error());
     }
