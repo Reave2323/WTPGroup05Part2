f5b84c83ae14768e79b71f22fadd67042507cabf
Reave2323 <jack.whitehall892@gmail.com>
2026-05-26 20:16:20 +1000

    created styling sheet for management pages

--- manage.php
+++ manage.php
@@ -14,15 +14,23 @@ if (!isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manager Portal</title>
+
+    <link rel="stylesheet" href="style/manage.css">
 </head>
 
 <body>
-    <h1>Manager Portal</h1>
-    <p>Welcome to the manager portal. Here you can manage job listings and view applications.</p>
-    <ul>
-        <li><a href="manage_jobs.php">Manage Job Listings</a></li>
-        <li><a href="applications.php">View Applications</a></li>
-    </ul>
+    <div class="content-area">
+        <div class="manager-portal">
+            <h1>Manager Portal</h1>
+            <p>Welcome to the manager portal. Here you can manage job listings and view applications.</p>
+            <div class="manager-list">
+                <ul class="manager-links">
+                    <li><a href="manage_jobs.php">Manage Job Listings</a></li>
+                    <li><a href="applications.php">View Applications</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
