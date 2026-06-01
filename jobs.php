e9c59554b9ff8e8a8a84d3ff3adf79f90171e4b0
105690697 <105690697@student.swin.edu.au>
2026-06-01 20:02:29 +1000

    removed embedded style

--- jobs.php
+++ jobs.php
@@ -83,7 +83,7 @@ $result = mysqli_query($conn, $query);
 				} else if ($row['category'] == 'Front End Development') {
 					$image = './images/FrontEnd.webp" style="transform: rotate(-90deg)"';
 				} else if ($row['category'] == 'Servicing Customers') {
-					$image = './images/customersupportgraphic.webp' width='100%' height='200px' object-fit='cover' object-position='center' border-radius: '12px';
+					$image = './images/customersupportgraphic.webp';
 				} else {
 					$image = 'DefaultJobImage.webp'; // image for Job categories
 				}
