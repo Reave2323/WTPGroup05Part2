d070fa0a4de0f8b41dde8c7fd1c88f3e2aa4f604
105690697 <105690697@student.swin.edu.au>
2026-06-01 20:03:34 +1000

    fixed bug

--- jobs.php
+++ jobs.php
@@ -83,7 +83,7 @@ $result = mysqli_query($conn, $query);
 				} else if ($row['category'] == 'Front End Development') {
 					$image = './images/FrontEnd.webp" style="transform: rotate(-90deg)"';
 				} else if ($row['category'] == 'Servicing Customers') {
-					$image = './images/customersupportgraphic.webp';
+					$image = './images/customersupportgraphic.webp"';
 				} else {
 					$image = 'DefaultJobImage.webp'; // image for Job categories
 				}
